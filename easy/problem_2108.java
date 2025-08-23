2fbe7fddf8a21a147c55051b27c4a54066c2ee25
Athul <athul.cs.dev@gmail.com>
2025-08-23 16:19:18 +0530

    aug 23

---
+++ easy/problem_2108.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for (String word : words) {
+            if (isPalindrome(word)) return word;
+        }
+        return "";
+    }
+
+    boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l++) != s.charAt(r--)) return false;
+        }
+        return true;
+    }
+}
