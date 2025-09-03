e063ea8eaf88d18322ae374d023b6f5a57ed1aa7
Athull <athul.cs.dev@gmail.com>
2025-09-04 01:10:14 +0530

    sep 4 2025 1.10

---
+++ easy/problem_2733.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findNonMinOrMax(int[] nums) {
+        if(nums.length<3) return -1;
+        Arrays.sort(nums);
+        return nums[1];
+
+    }
+}
+class Solution {
+    public int findNonMinOrMax(int[] nums) {
+        if (nums.length < 3) return -1;
+        int min = nums[0], max = nums[0];
+        for (int num : nums) {
+            if (num < min) min = num;
+            if (num > max) max = num;
+        }
+        for (int num : nums) {
+            if (num != min && num != max) return num;
+        }
+        return -1;
+    }
+}
